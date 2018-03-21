86da935187859fed81f1e2d1ada2a2a991990bbe
Luis Mercado <luis.mercado_1@nxp.com>
2018-03-21 09:51:12 -0600

    create mergeTables function

---
+++ R/mergeTables.R
@@ -0,0 +1,5 @@
+# Merge all data tables into one.
+# Handle unequal column position.
+# Handle unequal naming but similar content.
+# Provide similarity report among columns
+# Ask to merge if naming is not 100% accurate. Provide report.
