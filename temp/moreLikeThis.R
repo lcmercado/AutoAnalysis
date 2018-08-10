0b5f6a5b3025d1f112526804fd05d4414a992af7
Luis Mercado <luis.mercado_1@nxp.com>
2018-08-10 15:46:46 +0200

    Add a couple of placeholders for new functions

---
+++ temp/moreLikeThis.R
@@ -0,0 +1,5 @@
+#Function that takes strings or datasets as an argument
+#then goes to available APIs to find its source and the API arguments to obtain it
+#then tries to find the availability according to different fields: range, categories, etc.
+#
+#ARGUMENTS: input, list or group of APIs,
---
+++ temp/profiler.R
@@ -0,0 +1,5 @@
+#function to provide a unified set of measures per dataset
+#traditional> size, columns, types, range, etc.
+#correlations> correlation piramid.
+#sequentiallity> patterns detected in time series terms.
+#
